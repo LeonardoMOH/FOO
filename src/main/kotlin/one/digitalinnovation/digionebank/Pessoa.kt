77700949567cfdf7c3dcc37c1e6d9f3424e285ee
LeonardoMOH <leonardom.ohama@gmail.com>
2022-06-22 20:16:02 -0300

    New and updated "Pessoa" class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Leonardo"
+    var cpf: String = "123.456.789-10"
+
+        private set
+
+    fun pessoaInfo() = "Nome: $nome e CPF: $cpf"
+}
+
+fun main() {
+    val leonardo = Pessoa()
+    println(leonardo.pessoaInfo())
+}
