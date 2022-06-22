62e21a406435489974aa4146fa823a01f50367a5
LeonardoMOH <leonardom.ohama@gmail.com>
2022-06-22 20:45:21 -0300

    Banco, BancoTeste and Conta archives added

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,8 +3,9 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Leonardo"
     var cpf: String = "123.456.789-10"
+    private set
 
-        private set
+    constructor()
 
     fun pessoaInfo() = "Nome: $nome e CPF: $cpf"
 }
