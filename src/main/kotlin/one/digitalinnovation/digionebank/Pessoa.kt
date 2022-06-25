bf42382b986b14e7fdd6ed8bb5dffd5b0374ca1a
LeonardoMOH <leonardom.ohama@gmail.com>
2022-06-25 17:17:15 -0300

    Created new classes and their respective tests

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Leonardo"
-    var cpf: String = "123.456.789-10"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "Nome: $nome e CPF: $cpf"
-}
-
-fun main() {
-    val leonardo = Pessoa()
-    println(leonardo.pessoaInfo())
-}
+abstract class Pessoa (
+    val nome:String,
+    val cpf:String
+)
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/FuncionarioGerenteTeste.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Gerente
+
+fun main() {
+   val Maria = Gerente( "Maria do Carmo", "123.456.789-10", 5000.0)
+    ImprimeRelatorioFuncionario.imprime(Maria)
+
+}
